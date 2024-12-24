6679569d3510e7795a7291e7dc23670450c48d25
youyu <129youyu@gmail.com>
2024-12-24 15:33:35 +0800

    增加刪除後重新載入班級功能_新增後端方式

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -4,6 +4,9 @@ include_once "db.php";
 //處理刪除資料的請求
 $id=$_POST['id'];
 
+// 後端撈出id
+// $row=$Stu->find($id);
+
 $Stu->del($id);
 
 // 也可以寫成一行
@@ -11,4 +14,6 @@ $Stu->del($id);
 
 echo $_POST['classroom'];
 
+// 從後端方式顯示新的學生資料表
+// echo $row['classroom'];
 ?>
