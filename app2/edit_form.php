475d143a332387cc4e533209e8425892c50503d6
youyu <129youyu@gmail.com>
2024-12-27 14:22:08 +0800

    建立彈出視窗的編輯表單

--- app2/edit_form.php
+++ app2/edit_form.php
@@ -1,38 +1,47 @@
+<?php
+include_once "./api/db.php";
+// 工作上會做 data clean 不會直接把$_GET['id']放到function裡面
+$row=$Stu->find($_GET['id']);
+?>
+
 <!-- Modal -->
-<div class="modal fade" id="CreateModal" tabindex="-1" aria-labelledby="CreateModalLabel" aria-hidden="true">
+<div class="modal fade" id="EditModal" tabindex="-1" aria-labelledby="EditModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
 
             <!-- <form action="api/insert.php" method="post"> -->
             <form action="#" method="post">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5" id="CreateModalLabel">新增學生</h1>
+                    <h1 class="modal-title fs-5" id="EditModalLabel">新增學生</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div class="mb-3">
                         <label for="uni_id" class="form-label">學號</label>
-                        <input type="text" name="uni_id" class="form-control" id="uni_id">
+                        <input type="text" name="uni_id" class="form-control" id="uni_id" value="<?=$row['uni_id'];?>">
                     </div>
                     <div class="mb-3">
                         <label for="seat_num" class="form-label">座號</label>
-                        <input type="text" name="seat_num" class="form-control" id="seat_num">
+                        <input type="text" name="seat_num" class="form-control" id="seat_num"
+                            value="<?=$row['seat_num'];?>">
                     </div>
                     <div class="mb-3">
                         <label for="name" class="form-label">姓名</label>
-                        <input type="text" name="name" class="form-control" id="name">
+                        <input type="text" name="name" class="form-control" id="name" value="<?=$row['name'];?>">
                     </div>
                     <div class="mb-3">
                         <label for="classroom" class="form-label">班級</label>
-                        <input type="text" name="classroom" class="form-control" id="classroom">
+                        <input type="text" name="classroom" class="form-control" id="classroom"
+                            value="<?=$row['classroom'];?>">
                     </div>
                     <div class="mb-3">
                         <label for="major" class="form-label">科系</label>
-                        <input type="text" name="major" class="form-control" id="major">
+                        <input type="text" name="major" class="form-control" id="major" value="<?=$row['major'];?>">
+                        <input type="hidden" name="id" value="<?=$row['id'];?>">
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-primary" id='send'>儲存</button>
+                    <button type="button" class="btn btn-primary" id='send'>編輯</button>
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
                 </div>
             </form>
@@ -52,14 +61,14 @@ $("#send").on("click", function() {
     }
 
     // function()回呼函式
-    $.post("api/insert.php", formData, function() {
+    $.post("api/update.php", formData, function() {
         // 重新更新班級資料
         getClasses()
-        alert("新增完成")
-        CreateModal.hide();
+        alert("編輯完成")
+        EditModal.hide();
 
         // 全部清空，連記憶體都清空
-        CreateModal.dispose();
+        EditModal.dispose();
         // 讓html的modal消失
         $("#modal").html("");
 
